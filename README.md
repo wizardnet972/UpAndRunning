45fe38757708f44eaf1d2b0572ad429f2dc28033
WizardNet972 <wizardnet972@gmail.com>
2016-12-01 14:19:41 +0200

    add angular2

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # UpAndRunning
-just clone and run
+
+To run the examples:
+
+1. Clone this repo
+2. Enter to directory you want
+3. Run `http-server -p 3000`
+4. Open http://localhost:3000/
---
+++ angular2/app/app.component.ts
@@ -0,0 +1,15 @@
+import {Component} from '@angular/core';
+
+@Component({
+	selector: 'app',
+	template: `
+	  <div>
+      Welcome to Angular2 UP & RUNNING!
+      <br><br>
+      <a href="http://github.com/wizardnet972">My repo on github</a>
+    </div>`
+})
+export class AppComponent {
+
+  constructor() { }
+}
